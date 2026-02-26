8ebc224f1648108b07b2578415b6be7f920fc1da
rhodey <mike@rhodey.org>
2026-02-26 18:48:59 -0500

    big_int

--- README.md
+++ README.md
@@ -19,9 +19,9 @@ import { open } from 'sqlite-wasi'
 
 const db = open('/app/test.js.db')
 db.exec('drop table if exists demo')
-db.exec('create table demo (id integer, name text, note text, ratio real, big_id integer)')
+db.exec('create table demo (id integer, name text, note text, ratio real, big_int integer)')
 
-let statement = db.prepare('insert into demo (id, name, note, ratio, big_id) values (?, ?, ?, ?, ?)')
+let statement = db.prepare('insert into demo (id, name, note, ratio, big_int) values (?, ?, ?, ?, ?)')
 let info = statement.run([1, 'hello from js', null, 3.25, 9007199254740993n])
 console.log(info.changes, '== 1')
 console.log(info.lastInsertRowid, '== 1')
@@ -30,9 +30,9 @@ info = statement.run([2, 'hello from js', null, 3.25, 9007199254740993n])
 console.log(info.changes, '== 1')
 console.log(info.lastInsertRowid, '== 2')
 
-statement = db.prepare('select id, name, note, ratio, big_id from demo where id = ?')
+statement = db.prepare('select id, name, note, ratio, big_int from demo where id = ?')
 let row = statement.one([1])
-console.log(row) // >> { id: 1n, name: 'hello from js', note: null, ratio: 3.25, big_id: 9007199254740993n }
+console.log(row) // >> { id: 1n, name: 'hello from js', note: null, ratio: 3.25, big_int: 9007199254740993n }
 
 statement = db.prepare('select * from demo where 1 = ? order by id')
 let rows = statement.all([1])
--- package-js/test.js
+++ package-js/test.js
@@ -29,54 +29,54 @@ const test = () => {
   const db = open('/app/test.js.db')
   db.exec('drop table if exists demo')
 
-  let num = db.exec('create table demo (id integer, name text, note text, ratio real, big_id integer)')
+  let num = db.exec('create table demo (id integer, name text, note text, ratio real, big_int integer)')
   equals(num, 0n, 'create table no rows')
 
-  let statement = db.prepare('insert into demo (id, name, note, ratio, big_id) values (?, ?, ?, ?, ?)')
+  let statement = db.prepare('insert into demo (id, name, note, ratio, big_int) values (?, ?, ?, ?, ?)')
   let info = statement.run([1, 'hello from js', null, 3.25, 9007199254740993n])
   equals(info.changes, 1n, 'insert 1 row')
   equals(info.lastInsertRowid, 1n, 'row id 1')
   equals(true, statement.release(), 'release true')
   equals(false, statement.release(), 'release false')
 
-  statement = db.prepare('insert into demo (id, name, note, ratio, big_id) values (?, ?, ?, ?, ?)')
+  statement = db.prepare('insert into demo (id, name, note, ratio, big_int) values (?, ?, ?, ?, ?)')
   info = statement.run([2, 'hello from js', null, 3.25, 9007199254740993n])
   equals(info.changes, 1n, 'insert 1 row')
   equals(info.lastInsertRowid, 2n, 'row id 2')
   equals(true, statement.release(), 'release true')
   equals(false, statement.release(), 'release false')
 
-  const obj1 = { id: 1n, name: 'hello from js', note: null, ratio: 3.25, big_id: 9007199254740993n }
+  const obj1 = { id: 1n, name: 'hello from js', note: null, ratio: 3.25, big_int: 9007199254740993n }
   const obj2 = { ...obj1, id: 2n }
 
-  statement = db.prepare('select id, name, note, ratio, big_id from demo where id = 1')
+  statement = db.prepare('select id, name, note, ratio, big_int from demo where id = 1')
   let row = statement.one()
   equals(row, obj1, 'select 1 row A')
 
-  statement = db.prepare('select id, name, note, ratio, big_id from demo where id = ?')
+  statement = db.prepare('select id, name, note, ratio, big_int from demo where id = ?')
   row = statement.one([1])
   equals(row, obj1, 'select 1 row B')
 
-  statement = db.prepare('select id, name, note, ratio, big_id from demo where id = ?')
+  statement = db.prepare('select id, name, note, ratio, big_int from demo where id = ?')
   row = statement.one([2])
   equals(row, obj2, 'select 1 row C')
 
-  statement = db.prepare('select id, name, note, ratio, big_id from demo where id = 3')
+  statement = db.prepare('select id, name, note, ratio, big_int from demo where id = 3')
   row = statement.one()
   equals(row, null, 'select 1 row NULL')
 
-  statement = db.prepare('select id, name, note, ratio, big_id from demo order by id')
+  statement = db.prepare('select id, name, note, ratio, big_int from demo order by id')
   let rows = statement.all()
   equals(rows.length, 2, 'select 2 rows')
   equals(rows[0], obj1, 'select row id 1')
   equals(rows[1], obj2, 'select row id 2')
 
-  statement = db.prepare('select id, name, note, ratio, big_id from demo where id = ?')
+  statement = db.prepare('select id, name, note, ratio, big_int from demo where id = ?')
   rows = statement.all([1])
   equals(rows.length, 1, 'select 1 rows')
   equals(rows[0], obj1, 'select row id 1')
 
-  statement = db.prepare('select id, name, note, ratio, big_id from demo where id = ?')
+  statement = db.prepare('select id, name, note, ratio, big_int from demo where id = ?')
   rows = statement.all([3])
   equals(rows.length, 0, 'select 0 rows')
 
