3e67188ee2725f4c2993fd095b34fe6ffee7239b
rhodey <mike@rhodey.org>
2026-03-18 16:59:57 -0400

    Reduce Rust query parameter boilerplate again (#31)

--- example-rust/src/main.rs
+++ example-rust/src/main.rs
@@ -1,4 +1,4 @@
-use sqlite_wasm_wasi::{open, Value, NO_PARAMS};
+use sqlite_wasm_wasi::{open, Value};
 
 fn main() {
     if let Err(err) = example() {
@@ -8,49 +8,49 @@ fn main() {
 
 fn example() -> Result<(), sqlite_wasm_wasi::Error> {
     let db = open("/app/example.rust.db")?;
-    db.exec("drop table if exists example", &NO_PARAMS)?;
+    db.exec("drop table if exists example", &[])?;
     db.exec(
         "create table example (id integer, name text, note text, ratio real, big_int integer)",
-        &NO_PARAMS,
+        &[],
     )?;
 
     let mut insert =
         db.prepare("insert into example (id, name, note, ratio, big_int) values (?, ?, ?, ?, ?)")?;
-    let mut info = insert.run::<Value>(&[
-        1_i64.into(),
-        "hello from js".into(),
-        Value::Null,
-        3.25.into(),
-        9_007_199_254_740_993.into(),
+    let mut info = insert.run(&[
+        &1,
+        &"hello from js",
+        &Value::Null,
+        &3.25,
+        &9_007_199_254_740_993_i64,
     ])?;
     println!("{} == 1", info.changes);
     println!("{} == 1", info.last_insert_rowid);
 
-    info = insert.run::<Value>(&[
-        2_i64.into(),
-        "hello from js".into(),
-        Value::Null,
-        3.25.into(),
-        9_007_199_254_740_993.into(),
+    info = insert.run(&[
+        &2,
+        &"hello from js",
+        &Value::Null,
+        &3.25,
+        &9_007_199_254_740_993_i64,
     ])?;
     println!("{} == 1", info.changes);
     println!("{} == 2", info.last_insert_rowid);
 
     let mut select =
         db.prepare("select id, name, note, ratio, big_int from example where id = ?")?;
-    let row = select.one(&[1_i64])?;
+    let row = select.one(&[&1])?;
     println!("{:?}", row);
 
     select = db.prepare("select * from example where 1 = ? order by id")?;
-    let mut rows = select.all(&[1_i64])?;
+    let mut rows = select.all(&[&1])?;
     println!("{:?}", rows);
 
-    db.exec("drop table if exists txn", &NO_PARAMS)?;
-    db.exec("create table txn (id integer)", &NO_PARAMS)?;
+    db.exec("drop table if exists txn", &[])?;
+    db.exec("create table txn (id integer)", &[])?;
     insert = db.prepare("insert into txn (id) values (?)")?;
     let mut insert_many = db.transaction(|nums: Vec<i64>| {
         for num in nums {
-            insert.run(&[num])?;
+            insert.run(&[&num])?;
         }
         Ok(())
     });
@@ -59,7 +59,7 @@ fn example() -> Result<(), sqlite_wasm_wasi::Error> {
     insert_many(nums)?;
 
     select = db.prepare("select * from txn order by id")?;
-    rows = select.all(&NO_PARAMS)?;
+    rows = select.all(&[])?;
     println!("{:?}", rows);
     db.close()?;
 
